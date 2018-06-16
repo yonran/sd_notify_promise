76ed3b1b244977717051c55a4b3acc99d624c5cd
Yonathan Randolph <yonathan@gmail.com>
2018-06-15 18:36:04 -0700

    Add linux test

--- test.ts
+++ test.ts
@@ -2,6 +2,7 @@ import {sd_notify} from "./index"
 import {createSocket, ReceiveInfo} from "unix-dgram"
 // import {} from "mocha"
 import * as chai from "chai"
+import * as os from "os"
 import {unlinkSync} from "fs"
 
 describe("sd_notify", () => {
@@ -51,5 +52,26 @@ describe("sd_notify", () => {
         delete process.env.NOTIFY_SOCKET
         chai.expect(await sd_notify(false, "hello")).to.be.false
         chai.expect(await sd_notify(true, "hello")).to.be.false
+    })
+    it("should be able to connect to abstract namespace sockets on linux", async () => {
+        if (os.platform() !== "linux") return
+        const socket = createSocket('unix_dgram')
+        const name = "sd-notify-test"
+        process.env.NOTIFY_SOCKET = `@${name}`
+        socket.bind(`\0${name}`)
+        try {
+            const receivedMessagePromise = new Promise((resolve, reject) => {
+                socket.on("message", (buf: Buffer, rinfo: ReceiveInfo) => {
+                    resolve(buf)
+                })
+                socket.on("error", reject)
+            })
+            chai.expect(await sd_notify(false, "hello")).to.be.true
+            const msg = await receivedMessagePromise as Buffer
+            chai.expect(msg.toString("hex"))
+                .to.equal(Buffer.from("hello").toString("hex"))
+        } finally {
+            socket.close()
+        }
     })
 })
